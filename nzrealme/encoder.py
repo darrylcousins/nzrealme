7876a28926c027c60b98e0c601bbc803c5cea3c0
Darryl Cousins <darryljcousins@gmail.com>
2014-10-17 17:54:42 +1300

    removed unused file and updated docstring

--- nzrealme/encoder.py
+++ nzrealme/encoder.py
@@ -7,15 +7,13 @@ http://stackoverflow.com/questions/1089662/python-inflate-and-deflate-implementa
 
 The deflate/encode method is supposed to duplicate this bit of perl code::
 
-    my $xml    = $self->saml_request();
-    my $data   = '';
-    my $status = rawdeflate \$xml => \$data, Append => 0
-        or die "Can't compress request data: $RawDeflateError\n";
-
-    $data = encode_base64($data);
-    $data =~ s{[\r\n]}{}g;
-
-    return $data;
+    >>> my $xml    = $self->saml_request();
+    >>> my $data   = '';
+    >>> my $status = rawdeflate \$xml => \$data, Append => 0
+    >>>     or die "Can't compress request data: $RawDeflateError\n";
+    >>> $data = encode_base64($data);
+    >>> $data =~ s{[\r\n]}{}g;
+    >>> return $data;
 
 My investigation found that ``rawdeflate`` compresses data to RFC 1951.
 
@@ -27,7 +25,6 @@ On deflate/compress: strip the first two bytes (header) and the last four bytes
 
 On inflate/decompress: there is a second argument for window size. If this
 value is negative it suppresses headers.::
-
 
     >>> xml = '<Header><Body>The content</Body></Header>'
     >>> b64 = deflate_and_base64_encode(xml)
